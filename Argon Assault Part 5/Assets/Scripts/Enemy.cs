51232f3a8b1421e937bb2eb6e4aef4d9d07ff875
mcdani978 <mcdani978@morrisville.edu>
2024-11-17 19:45:19 -0500

    Set up enemy prefab with Rigidbody components.

--- Argon Assault Part 5/Assets/Scripts/Enemy.cs
+++ Argon Assault Part 5/Assets/Scripts/Enemy.cs
@@ -15,6 +15,13 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        AddRigidBody();
+    }
+
+    void AddRigidBody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     void OnParticleCollision(GameObject other)
